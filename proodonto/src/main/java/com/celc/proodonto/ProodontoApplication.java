a06ae4d8df5ad199d9d20bfb887371561a273556
Eduardo <ceduardolima2000@gmail.com>
2023-09-07 19:36:51 -0300

    Criando projeto spring boot

---
+++ proodonto/src/main/java/com/celc/proodonto/ProodontoApplication.java
@@ -0,0 +1,13 @@
+package com.celc.proodonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProodontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProodontoApplication.class, args);
+	}
+
+}
---
+++ proodonto/src/test/java/com/celc/proodonto/ProodontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celc.proodonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProodontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
