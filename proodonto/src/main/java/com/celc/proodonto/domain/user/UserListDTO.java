7e60198548e37a4684354b7707a07b6f07adebff
Eduardo <ceduardolima2000@gmail.com>
2023-09-08 21:29:11 -0300

    Adicionando campo ID na UserListDTO

--- proodonto/src/main/java/com/celc/proodonto/domain/user/UserListDTO.java
+++ proodonto/src/main/java/com/celc/proodonto/domain/user/UserListDTO.java
@@ -3,12 +3,13 @@ package com.celc.proodonto.domain.user;
 import com.celc.proodonto.domain.address.AddressDTO;
 
 public record UserListDTO(
+        Long id,
         String name,
         String email,
         String cpf,
         AddressDTO address
 ) {
     public UserListDTO(User user) {
-        this(user.getName(), user.getEmail(), user.getCpf(), new AddressDTO(user.getAddress()));
+        this(user.getId(), user.getName(), user.getEmail(), user.getCpf(), new AddressDTO(user.getAddress()));
     }
 }
