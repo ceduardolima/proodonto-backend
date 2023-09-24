2fb965134364c95f5bcd981e60e1d47670157fd1
Eduardo <ceduardolima2000@gmail.com>
2023-09-24 19:31:38 -0300

    Criando repositório para fazer a conexão com a tabela Records do banco de dados

---
+++ proodonto/src/main/java/com/celc/proodonto/domain/records/RecordRepository.java
@@ -0,0 +1,6 @@
+package com.celc.proodonto.domain.records;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
